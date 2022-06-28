29a271e6156aead1b9fad6a3cc142ebced23b71c
Polifir <ulanooov@gmail.com>
2022-06-28 11:07:41 +0300

    add: add table folder

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,14 +3,15 @@ import React from "react";
 import { CreateBtn } from "./CreateBtn/CreateBtn.js";
 import { Title } from "./Title/Title.js";
 import styles from "./Main.module.sass";
+import { Table } from "./Table/Table.js";
 
 export const Main = () => {
   return (
-    <main className={styles.main}>
+    <div className={styles.main}>
       <header className={styles.content}>
         <Title />
         <CreateBtn />
       </header>
-    </main>
+    </div>
   );
 };
